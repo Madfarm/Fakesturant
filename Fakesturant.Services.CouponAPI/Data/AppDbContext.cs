9f42721282768d45046f3fdd04ad3158cbf606e3
Madfarm <anthony.scott1005@gmail.com>
2023-08-21 22:06:31 -0400

    Add code to seed CouponDb and add middleware that checks for pending migrations and runs them when the application is run

---
+++ Fakesturant.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Fakesturant.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fakesturant.Services.CouponAPI.NewFolder
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "TENNY",
+                DiscountAmount = 10,
+                MinAmount = 25
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "LBRDAY",
+                DiscountAmount = 15,
+                MinAmount = 30
+            });
+        }
+    }
+}
--- Fakesturant.Services.CouponAPI/Migrations/20230822020232_SeedCouponTables.Designer.cs
+++ Fakesturant.Services.CouponAPI/Migrations/20230822020232_SeedCouponTables.Designer.cs
@@ -3,6 +3,7 @@ using Fakesturant.Services.CouponAPI.NewFolder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fakesturant.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230822020232_SeedCouponTables")]
+    partial class SeedCouponTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
