b38ac0849f990bfbdac587b9dd6648109b2d5cc5
Madfarm <anthony.scott1005@gmail.com>
2023-08-26 00:13:58 -0400

    MVC Client App | Refactor Request DTO to use enum for the HTTP Verb

--- Fakesturant.Web/Models/DTOs/RequestDto.cs
+++ Fakesturant.Web/Models/DTOs/RequestDto.cs
@@ -1,8 +1,10 @@
-﻿namespace Fakesturant.Web.Models.DTOs
+﻿using static Fakesturant.Web.Utility.SD;
+
+namespace Fakesturant.Web.Models.DTOs
 {
     public class RequestDto
     {
-        public string ApiType { get; set; } = "GET";
+        public ApiType ApiType { get; set; } = ApiType.GET;
         public string Url { get; set; }
         public object Data { get; set; }
         public string AccessTables { get; set; }
---
+++ Fakesturant.Web/Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace Fakesturant.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET, POST, PUT, DELETE
+        }
+    }
+}
