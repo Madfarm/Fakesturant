c1fe9b6c13165e869679de48bed48680dd63f3db
Madfarm <anthony.scott1005@gmail.com>
2023-08-26 00:12:02 -0400

    MVC Client App | Create Request and Response DTOs

---
+++ Fakesturant.Web/Models/DTOs/RequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Fakesturant.Web.Models.DTOs
+{
+    public class RequestDto
+    {
+        public string ApiType { get; set; } = "GET";
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessTables { get; set; }
+    }
+}
