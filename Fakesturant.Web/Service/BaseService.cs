067eb207e79870330092b7c890e0616940cbc64c
Madfarm <anthony.scott1005@gmail.com>
2023-08-29 20:20:43 -0400

    MVC Client App | Inject the HttpClientFactory into the base service

---
+++ Fakesturant.Web/Service/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using Fakesturant.Web.Models.DTOs;
+using Fakesturant.Web.Service.IService;
+
+namespace Fakesturant.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
