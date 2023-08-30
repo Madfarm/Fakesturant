08c768d8c505e2094b0ed6b380856a831fa00ebb
Madfarm <anthony.scott1005@gmail.com>
2023-08-29 20:52:01 -0400

    MVC Client App | Create CouponService interface

--- Fakesturant.Web/Service/ICouponService.cs
+++ Fakesturant.Web/Service/ICouponService.cs
@@ -1,7 +1,17 @@
-﻿namespace Fakesturant.Web.Service
+﻿using Fakesturant.Web.Models;
+using Fakesturant.Web.Models.DTOs;
+
+namespace Fakesturant.Web.Service
 {
     public interface ICouponService
     {
+        Task<ResponseDto?> GetCouponAsync(string CouponCode);
+        Task<ResponseDto?> GetAllCoupon();
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int id);
+
 
     }
 }
