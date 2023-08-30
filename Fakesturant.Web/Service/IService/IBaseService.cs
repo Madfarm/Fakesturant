34dc84160d33013eefdcb0f3c7e29c2f4c3c23e4
Madfarm <anthony.scott1005@gmail.com>
2023-08-29 20:17:25 -0400

    MVC Client App | Finish interface for our services to implement

--- Fakesturant.Web/Service/IService/IBaseService.cs
+++ Fakesturant.Web/Service/IService/IBaseService.cs
@@ -1,7 +1,9 @@
-﻿namespace Fakesturant.Web.Service.IService
+﻿using Fakesturant.Web.Models.DTOs;
+
+namespace Fakesturant.Web.Service.IService
 {
     public interface IBaseService
     {
-        
+       Task<ResponseDto?> SendAsync(RequestDto requestDto);
     }
 }
