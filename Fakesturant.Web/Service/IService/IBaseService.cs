99232839e99d56a7e3259d6732012d2da81630ba
Madfarm <anthony.scott1005@gmail.com>
2023-09-08 23:11:53 -0400

    Update interface for base service

--- Fakesturant.Web/Service/IService/IBaseService.cs
+++ Fakesturant.Web/Service/IService/IBaseService.cs
@@ -1,7 +1,8 @@
 ﻿using Fakesturant.Web.Models.DTOs;
 
 namespace Fakesturant.Web.Service.IService
-{
+{   
+    //Interface for base service
     public interface IBaseService
     {
        Task<ResponseDto?> SendAsync(RequestDto requestDto);
