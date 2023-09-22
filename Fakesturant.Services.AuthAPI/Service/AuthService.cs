1ffac41a5a400c212f5be2bdabb9a83f12cb136c
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 12:43:30 -0400

    Auth API | Scaffold the Auth Service

---
+++ Fakesturant.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using Fakesturant.Services.AuthAPI.Models.Dto;
+using Fakesturant.Services.AuthAPI.Service.IService;
+
+namespace Fakesturant.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Regiser(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
