1502d324fd60e93c7c5e3d5b7a68b66f5cd025c6
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 11:50:53 -0400

    Auth API | Scaffold auth controller

---
+++ Fakesturant.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fakesturant.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+    }
+}
