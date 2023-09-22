6be57500ae2f64f69741043fd2b62e70ff53c584
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 11:46:17 -0400

    Auth API | Add migration with new User model

--- Fakesturant.Services.AuthAPI/Migrations/20230922154524_AddNameToIdentityUsers.Designer.cs
+++ Fakesturant.Services.AuthAPI/Migrations/20230922154524_AddNameToIdentityUsers.Designer.cs
@@ -4,6 +4,7 @@ using Fakesturant.Services.AuthAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fakesturant.Services.AuthAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230922154524_AddNameToIdentityUsers")]
+    partial class AddNameToIdentityUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
