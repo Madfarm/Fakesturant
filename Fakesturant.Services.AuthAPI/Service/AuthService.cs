8cc74f0496566271ec29a04b44c50b9a6d159c64
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 12:47:38 -0400

    Auth API | Inject the dbcontext and required helper methods into the auth service

--- Fakesturant.Services.AuthAPI/Service/AuthService.cs
+++ Fakesturant.Services.AuthAPI/Service/AuthService.cs
@@ -1,10 +1,23 @@
-﻿using Fakesturant.Services.AuthAPI.Models.Dto;
+﻿using Fakesturant.Services.AuthAPI.Data;
+using Fakesturant.Services.AuthAPI.Models;
+using Fakesturant.Services.AuthAPI.Models.Dto;
 using Fakesturant.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
 
 namespace Fakesturant.Services.AuthAPI.Service
 {
     public class AuthService : IAuthService
     {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager; 
+        }
         public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             throw new NotImplementedException();
