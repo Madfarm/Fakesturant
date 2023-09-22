be74b5451471f653178ca1e2c304789c9102773f
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 11:59:03 -0400

    Auth API | Add User Dto

---
+++ Fakesturant.Services.AuthAPI/Models/Dto/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Fakesturant.Services.AuthAPI.Models.Dto
+{
+    public class UserDto
+    {
+        // IDs in Identity are Guids so it must be a string here
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
