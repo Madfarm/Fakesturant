78b1b8266702b4d2abfa945a5a3b4e01ffaa831a
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 12:42:15 -0400

    Auth API | Add an interface for AuthService

---
+++ Fakesturant.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Fakesturant.Services.AuthAPI.Models.Dto;
+
+namespace Fakesturant.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Regiser(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
