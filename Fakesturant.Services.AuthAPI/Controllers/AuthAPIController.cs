af7f8603b39ecdf8015b424246400e205d0b82a1
Madfarm <anthony.scott1005@gmail.com>
2023-09-27 22:03:03 -0400

    Auth API | Handel bad requests into the login endpoint

--- Fakesturant.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Fakesturant.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -36,9 +36,16 @@ namespace Fakesturant.Services.AuthAPI.Controllers
         
         //Missing attributes
         [HttpPost("login")]
-        public async Task<IActionResult> Login()
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
         {
-            return Ok();
+            var loginResponse = await _authService.Login(model);
+            
+            if (loginResponse.User == null)
+            {
+                _response.IsSuccessful = false;
+                _response.Message = "Username or password is incorrect";
+                return BadRequest(_response);
+            }
         }
 
     }
