be26da607ad4dae5b7e0cf8bb67533a9be2f91da
Madfarm <anthony.scott1005@gmail.com>
2023-10-04 00:24:43 -0400

    Auth API | Create JwtTokenGenerator interface

---
+++ Fakesturant.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Fakesturant.Services.AuthAPI.Models;
+
+namespace Fakesturant.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
