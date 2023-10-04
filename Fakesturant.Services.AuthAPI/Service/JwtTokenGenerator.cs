36eac3bb6bcc92148b1b1774ecfac6a39bdd1e38
Madfarm <anthony.scott1005@gmail.com>
2023-10-04 00:26:57 -0400

    Auth API | Inject the JwtOptions into the JwtTokenGenerator

---
+++ Fakesturant.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Fakesturant.Services.AuthAPI.Models;
+using Fakesturant.Services.AuthAPI.Service.IService;
+
+namespace Fakesturant.Services.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtTokenGenerator(JwtOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
