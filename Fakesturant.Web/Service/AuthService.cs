382e8700c8deabc06445495a6d0fa0ab0f1631e7
Madfarm <anthony.scott1005@gmail.com>
2023-10-11 19:17:41 -0400

    Client App | Scaffold the auth service

---
+++ Fakesturant.Web/Service/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using Fakesturant.Web.Models;
+using Fakesturant.Web.Models.DTOs;
+using Fakesturant.Web.Service.IService;
+
+namespace Fakesturant.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        public Task<ResponseDto?> AssignRole(RegistrationRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> Login(LoginRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> Register(RegistrationRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
