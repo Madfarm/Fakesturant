a9984b4b01c79aef9c6affeaf301463ca317b89e
Madfarm <anthony.scott1005@gmail.com>
2023-10-11 19:16:54 -0400

    Client App | Create the interface for the auth service

---
+++ Fakesturant.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Fakesturant.Web.Models;
+using Fakesturant.Web.Models.DTOs;
+
+namespace Fakesturant.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> Register(RegistrationRequestDto request);
+        Task<ResponseDto?> Login(LoginRequestDto request);
+        Task<ResponseDto?> AssignRole(RegistrationRequestDto request);
+    }
+}
