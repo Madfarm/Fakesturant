d6878c89201bfd43e96b2c4dedd22c042b0be436
Madfarm <anthony.scott1005@gmail.com>
2023-10-23 19:09:36 -0400

    Client App | Create interface for token provider

---
+++ Fakesturant.Web/Service/IService/ITokenProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Fakesturant.Web.Service.IService
+{
+    public interface ITokenProvider
+    {
+        void SetToken(string token);
+        string? GetToken();
+        void ClearToken();
+    }
+}
