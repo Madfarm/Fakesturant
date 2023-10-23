9dcedcf9ab43efbab4fd066d1af4edb35396645a
Madfarm <anthony.scott1005@gmail.com>
2023-10-23 19:52:52 -0400

    Client App | Add logout action method to auth controller

--- Fakesturant.Web/Controllers/AuthController.cs
+++ Fakesturant.Web/Controllers/AuthController.cs
@@ -99,9 +99,11 @@ namespace Fakesturant.Web.Controllers
         }
 
 
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            _tokenProvider.ClearToken();
+            return RedirectToAction("Index", "Home");
         }
 
         private async Task SignInUser(LoginResponseDto model)
