9dda71f3eb3830224b5e900fbe5100753eb0bb71
Madfarm <anthony.scott1005@gmail.com>
2023-10-23 19:10:25 -0400

    Client App | Scaffold TokenProvider service

---
+++ Fakesturant.Web/Service/TokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using Fakesturant.Web.Service.IService;
+
+namespace Fakesturant.Web.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        public void ClearToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
