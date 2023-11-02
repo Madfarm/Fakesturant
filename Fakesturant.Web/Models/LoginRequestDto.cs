40566001be024a24dc6ba0ebcd1589b8ebb6cc61
Madfarm <anthony.scott1005@gmail.com>
2023-11-01 21:39:28 -0400

    Client App | Make data in Login and Register request dtos required

--- Fakesturant.Web/Models/LoginRequestDto.cs
+++ Fakesturant.Web/Models/LoginRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Fakesturant.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fakesturant.Web.Models
 {
     public class LoginRequestDto
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
