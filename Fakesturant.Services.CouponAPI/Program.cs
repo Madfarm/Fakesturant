d8295e8e5576d0c259c542fa576d0bb84f316956
Madfarm <anthony.scott1005@gmail.com>
2023-11-22 21:42:04 -0500

    CouponAPI | Update program.cs to correctly retrieve the data to be validated against

--- Fakesturant.Services.CouponAPI/Program.cs
+++ Fakesturant.Services.CouponAPI/Program.cs
@@ -1,7 +1,10 @@
 using AutoMapper;
 using Fakesturant.Services.CouponAPI;
 using Fakesturant.Services.CouponAPI.NewFolder;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +25,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var secret = builder.Configuration.GetValue<string>("ApiSettings:JwtOptions:Secret");
+var issuer = builder.Configuration.GetValue<string>("AppSettings:JwtOptions:Issuer");
+var audience = builder.Configuration.GetValue<string>("AppSettings:JwtOptions:Audience");
+
+var key = Encoding.ASCII.GetBytes(secret);
+
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        ValidateAudience = true
+    };
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
